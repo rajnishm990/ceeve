1f4360379e60e99b4a70c2939da9170416aaf193
rajnish <rajnishm990@gmail.com>
2025-11-10 12:49:05 +0530

    add main entrypoint for app

---
+++ app/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware 
+from app.core.config import settings 
+from app.core.database import engine , Base
+
+# Import routers (will add them later)
+# from app.routers import auth_router, resume_router 
+
+# Create DB tables (for dev only — in prod use Alembic)
+Base.metadata.create_all(bind=engine) 
+
+#init app 
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    debug=settings.DEBUG
+)
+
+# CORS setup
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Include routers
+# app.include_router(auth_router.router, prefix="/auth", tags=["Auth"])
+# app.include_router(resume_router.router, prefix="/resume", tags=["Resume"])
+
+@app.get("/")
+def root():
+    return {"message": f"Welcome to {settings.PROJECT_NAME} API"}
